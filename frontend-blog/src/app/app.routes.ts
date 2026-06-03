3ec4b71928842a9d90f8578ce7ebd56595c338c7
igmar2698 <250371007@konak.edu.tr>
2026-06-03 11:53:24 +0300

    Rotalar jilet gibi duzeltildi

--- frontend-blog/src/app/app.routes.ts
+++ frontend-blog/src/app/app.routes.ts
@@ -1,8 +1,12 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { PostDetailComponent } from './post-detail/post-detail';
+import { AllPostsComponent } from './all-posts/all-posts';
+import { About } from './about/about';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Client
-  }
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'post-detail/:id', component: PostDetailComponent },
+  { path: 'all-posts', component: AllPostsComponent },
+  { path: 'about', component: About },
 ];
