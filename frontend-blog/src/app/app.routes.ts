80b84e19c147df0561e1bc2af8ce1498d9076afb
igmar2698 <250371007@konak.edu.tr>
2026-06-03 11:47:28 +0300

    Prerender hatasi Client moduna alinarak cozuldü

--- frontend-blog/src/app/app.routes.ts
+++ frontend-blog/src/app/app.routes.ts
@@ -1,12 +1,8 @@
-import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { PostDetailComponent } from './post-detail/post-detail';
-import { AllPostsComponent } from './all-posts/all-posts';
-import { About } from './about/about';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'post-detail/:id', component: PostDetailComponent },
-  { path: 'all-posts', component: AllPostsComponent },
-  { path: 'about', component: About },
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Client
+  }
 ];
